adec24cf1da85a929d44e06b77b733f2dc0242fb
Dmitriy <dmitriy.tereshchenko1989@gmail.com>
2023-03-20 10:44:00 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Can_Place_Flowers
+{
+    internal class Program
+    {
+        public class PlaceCanFlowers
+        {
+            public bool CanPlaceFlowers(int[] flowerbed, int n)
+            {
+                int placeCount = 0;
+                for (int i = 0; i < flowerbed.Length; ++i)
+                {
+                    if (flowerbed[i] == 0)
+                    {
+                        bool emptyLeftPlace = i == 0 || flowerbed[i - 1] == 0;
+                        bool emptyRightPlace = i == flowerbed.Length - 1 || flowerbed[i + 1] == 0;
+                        if (emptyLeftPlace && emptyRightPlace)
+                        {
+                            flowerbed[i] = 1;
+                            ++placeCount;
+                            if (placeCount >= n)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                return placeCount >= n;
+            }
+        }
+        static void Main(string[] args)
+        {
+            PlaceCanFlowers placeCanFlowers = new();
+            Console.WriteLine(placeCanFlowers.CanPlaceFlowers(new int[] { 1, 0, 0, 0, 1 }, 1));
+            Console.WriteLine(placeCanFlowers.CanPlaceFlowers(new int[] { 1, 0, 0, 0, 1 }, 2));
+        }
+    }
+}
